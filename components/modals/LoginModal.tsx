88773dee137a83c51632ea27e01e61f3fbe17306
vikash <vikash.tiwari@underscoretec.com>
2023-04-08 13:07:59 +0530

    name changed

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -60,7 +60,7 @@ const LoginModal = () => {
 
   const footerContent = (
     <div className="text-neutral-400 text-center mt-4">
-      <p>First time using Twitter?
+      <p>First time using Tweet?
         <span 
           onClick={onToggle} 
           className="
