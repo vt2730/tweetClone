f3f1e2bf11ec671e996c5213e936b698196d35db
vikash <vikash.tiwari@underscoretec.com>
2023-04-08 09:49:47 +0530

    user profile done

--- components/layout/FollowBar.tsx
+++ components/layout/FollowBar.tsx
@@ -1,20 +1,21 @@
-// import useUsers from '@/hooks/useUsers';
 
-// import Avatar from '../Avatar';
+import Avatar from '../Avatar';
+
+import useUsers from "@/hooks/useUsers";
 
 const FollowBar = () => {
-//   const { data: users = [] } = useUsers();
+  const { data: users = [] } = useUsers();
 
-//   if (users.length === 0) {
-//     return null;
-//   }
+  if (users.length === 0) {
+    return null;
+  }
 
   return (
     <div className="px-6 py-4 hidden lg:block">
       <div className="bg-neutral-800 rounded-xl p-4">
         <h2 className="text-white text-xl font-semibold">Who to follow</h2>
         <div className="flex flex-col gap-6 mt-4">
-          {/* {users.map((user: Record<string, any>) => (
+          {users.map((user: Record<string, any>) => (
             <div key={user.id} className="flex flex-row gap-4">
               <Avatar userId={user.id} />
               <div className="flex flex-col">
@@ -22,7 +23,7 @@ const FollowBar = () => {
                 <p className="text-neutral-400 text-sm">@{user.username}</p>
               </div>
             </div>
-          ))} */}
+          ))}
         </div>
       </div>
     </div>
