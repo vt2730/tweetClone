8ca293e8f3f001cb624d309876835d216ff790aa
vikash <vikash.tiwari@underscoretec.com>
2023-04-08 11:42:43 +0530

    edit profile and image upload done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,17 +6,19 @@ import LoginModal from '@/components/modals/LoginModal'
 import RegisterModal from '@/components/modals/RegisterModal'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import EditModal from '@/components/modals/EditModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <Toaster/>
-    <RegisterModal/>
-    <LoginModal/>
+      <Toaster />
+      <EditModal />
+      <RegisterModal />
+      <LoginModal />
 
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </SessionProvider>
    
   )
